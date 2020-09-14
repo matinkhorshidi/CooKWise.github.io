a48beccec90ce1047bb3322640f8306fd470f5d4
matin khorshidi <khorshidimatin@gmail.comm>
2020-09-14 15:22:54 +0430

    animation nav smooth

--- resources/js/script.js
+++ resources/js/script.js
@@ -10,62 +10,60 @@ $(document).ready(function () {
     }, {
         offset: '200px;'
     });
-//        $('.js--scroll-to-start').click(function () {
-//            $('html, body').animate({
-//                scrollTop: $('.js--section-features').offset().top
-//            }, 1000);
-//        });
+    //        $('.js--scroll-to-start').click(function () {
+    //            $('html, body').animate({
+    //                scrollTop: $('.js--section-features').offset().top
+    //            }, 1000);
+    //        });
 
 
     /* Scroll on buttons */
     $('.js--scroll-to-plans').click(function () {
         $('html, body').animate({
-            scrollTop: $('.js--section-plans').offset().top-50
+            scrollTop: $('.js--section-plans').offset().top - 50
         }, 800);
     });
 
-
+    
     //  Navigation smooth scroling
 
-        $('a[href*="#"]')
-            // Remove links that don't actually link to anything
-            .not('[href="#"]')
-            .not('[href="#0"]')
-            .click(function (event) {
-                event.preventDefault();
-                // On-page links
-                if (
-                    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-                    location.hostname == this.hostname
-                ) {
-                    // Figure out element to scroll to
-                    var target = $(this.hash);
-                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                    // Does a scroll target exist?
-                    if (target.length) {
-                        // Only prevent default if animation is actually gonna happen
-                        event.preventDefault();
-                        $('html, body').animate({
-                            scrollTop: target.offset().top-40
-                        }, 1000, function () {
-                            // Callback after animation
-                            // Must change focus!
-                            var $target = $(target);
-                            $target.focus();
-                            if ($target.is(":focus")) { // Checking if the target was focused
-                                return false;
-                            } else {
-                                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                                $target.focus(); // Set focus again
-                            };
-                        });
-                    }
+    // Select all links with hashes
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            event.preventDefault();
+
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top -40
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
                 }
-            });
-
-
-
-
+            }
+        });
 
     // ------- animate ---------
 
@@ -94,11 +92,11 @@ $(document).ready(function () {
     });
 
 
-    $(".js--wp-4").hover(function(){
-        $( this ).addClass( "plan-box-hover" );
-        }, function(){
-        $( this ).removeClass( "plan-box-hover" );
-      });
+    $(".js--wp-4").hover(function () {
+        $(this).addClass("plan-box-hover");
+    }, function () {
+        $(this).removeClass("plan-box-hover");
+    });
 
     $('.js--nav-icon').click(function (direction) {
         //        $('.js--main-nav').slideToggle(300);
